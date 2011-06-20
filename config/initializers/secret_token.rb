7631ea95da73d942c54f2c70c0efb8ce7fa93841
Zack Yap <zackyap@gmail.com>
2011-06-20 22:29:50 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodayEatWhat::Application.config.secret_token = '8a34a5a4c14e1ff2b788a520b54d919aef973470d4dbbc3cd11039d7c281267aeb832498ab20196b9d5df8e28454672a7d4bf34b2596638d76179107d9132211'
