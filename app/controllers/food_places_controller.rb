513253b1aaed3b2dd97cd169ce024322564db77f
Zack Yap <zackyap@gmail.com>
2011-06-21 16:14:08 +0800

    Removed excess commented code

--- app/controllers/food_places_controller.rb
+++ app/controllers/food_places_controller.rb
@@ -1,21 +1,17 @@
 class FoodPlacesController < ApplicationController
-  # before_filter :all_places
+  before_filter :all_places
 
   def index
-    # @food_place = FoodPlace.find(:id => rand(FoodPlace.find(:all).count))
-    # @place = @food_places[rand(@food_places.count)]
-  
+
   end
 
   def show
-    # @place = FoodPlace.find(:first, :offset => rand(FoodPlace.count :all))
-    @random_place = FoodPlace.random
     @place = @random_place.place
     @disgust_text = @random_place.disgust_text
   end
 
-# private
-#   def all_places
-#     @all_places = FoodPlace.find(:all)
-#   end
+private
+  def random_place
+    @random_place = FoodPlace.random
+  end
 end
--- app/models/food_place.rb
+++ app/models/food_place.rb
@@ -1,4 +1,6 @@
 class FoodPlace < ActiveRecord::Base
+	attr_accessor :place, :disgust_text
+
   def self.random
 
     self.find(:first, :offset => rand(self.all.size))
