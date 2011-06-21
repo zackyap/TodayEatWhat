59e503b1622401b7685497c02ed802d22e4768c0
Zack Yap <zackyap@gmail.com>
2011-06-22 06:50:20 +0800

    styled up the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title
     @title = "Today Eat What Ah??"
   end
+
+  def google_font_link_tag(family)
+    tag('link', {:rel => :stylesheet, :type => Mime::CSS, :href => "http://fonts.googleapis.com/css?family=#{family}"}, false, false)
+  end
 end
